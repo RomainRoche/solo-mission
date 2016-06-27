4f4bc854825cd8e40a6759ed0cd87c1b7fbba3c7
Romain Roche <romain.roche@news-republic.com>
2016-06-27 15:16:22 +0200

    chg: pause the game when background

--- solo-mission/AppDelegate.swift
+++ solo-mission/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SpriteKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,10 +28,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        if let view = self.window?.rootViewController?.view as? SKView {
+            view.isPaused = true
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        if let view = self.window?.rootViewController?.view as? SKView {
+            view.isPaused = false
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
